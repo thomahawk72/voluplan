6870f19a16cfddb5e40d5210f44033a3f2cb4140
thomas almnes <thomas@almnes.no>
2025-10-10 19:42:07 +0200

    Fix wildcard route for Express 5

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
